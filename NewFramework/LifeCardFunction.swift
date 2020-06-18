9d31aef3a92d01469f8b3fe95712be5ce6b68a0e
David Bui <haibuihoang@vccorp.vn>
2020-06-18 10:54:21 +0700

    ABC

--- NewFramework/LifeCardFunction.swift
+++ NewFramework/LifeCardFunction.swift
@@ -15,4 +15,12 @@ class LifeCardFunction {
         
     }
     
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
 }
