d8a3de539db773e93716f23d0e5131717f0cd073
David Bui <haibuihoang@vccorp.vn>
2020-06-18 11:15:33 +0700

    abc

---
+++ NewFramework/Project/ReplaceMe.swift
@@ -0,0 +1,21 @@
+//
+//  ReplaceMe.swift
+//  NewFramework
+//
+//  Created by BigSun on 6/18/20.
+//  Copyright © 2020 BigSun. All rights reserved.
+//
+
+import Foundation
+
+class SwiftyFunction {
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
